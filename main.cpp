98e74672d77a201ebd4f62cb61fb4d069cd45830
Oleg Baskakov <spetz911@gmail.com>
2015-04-24 18:52:32 -0500

    fix main.cpp to use shaders and GLFW

--- main.cpp
+++ main.cpp
@@ -1,48 +1,217 @@
+#include <cstdio>
+#include <vector>
+
+// Include GLFW
+#define GLFW_INCLUDE_GLCOREARB
 #include <GLFW/glfw3.h>
 
-#include <cstdio>
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace glm;
 
 
-int main(void)
+const char vertexShaderCode[] =
+        "#version 330 core                                                   \n"
+        "// Input vertex data, different for all executions of this shader.  \n"
+        "layout(location = 0) in vec3 vertexPosition_modelspace;             \n"
+        "                                                                    \n"
+        "uniform mat4 MVP;                                                   \n"
+        "                                                                    \n"
+        "void main() {                                                       \n"
+        "   gl_Position =  MVP * vec4(vertexPosition_modelspace,1);          \n"
+        "}                                                                   \n";
+
+
+const char fragmentShaderCode[] =
+        "#version 330 core          \n"
+        "out vec3 color;            \n"
+        "                           \n"
+        "void main() {              \n"
+        "   color = vec3(1,0,0);    \n"
+        "}                          \n";
+
+GLuint loadShaders(const char * vertex_file_data, const char * fragment_file_data) {
+
+    // Create the shaders
+    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+
+    // Compile Vertex Shader
+    printf("Compiling Vertex shader\n");
+    glShaderSource(VertexShaderID, 1, &vertex_file_data , NULL);
+    glCompileShader(VertexShaderID);
+
+    // Check Vertex Shader
+    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> VertexShaderErrorMessage((unsigned long) (InfoLogLength+1));
+        glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+        printf("%s\n", &VertexShaderErrorMessage[0]);
+    }
+
+
+
+    // Compile Fragment Shader
+    printf("Compiling Fragment shader\n");
+    glShaderSource(FragmentShaderID, 1, &fragment_file_data , NULL);
+    glCompileShader(FragmentShaderID);
+
+    // Check Fragment Shader
+    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> FragmentShaderErrorMessage((unsigned long) (InfoLogLength+1));
+        glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+        printf("%s\n", &FragmentShaderErrorMessage[0]);
+    }
+
+
+
+    // Link the program
+    printf("Linking program\n");
+    GLuint ProgramID = glCreateProgram();
+    glAttachShader(ProgramID, VertexShaderID);
+    glAttachShader(ProgramID, FragmentShaderID);
+    glLinkProgram(ProgramID);
+
+    // Check the program
+    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> ProgramErrorMessage((unsigned long) (InfoLogLength+1));
+        glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+        printf("%s\n", &ProgramErrorMessage[0]);
+    }
+
+    glDeleteShader(VertexShaderID);
+    glDeleteShader(FragmentShaderID);
+
+    return ProgramID;
+}
+
+
+
+int main( void )
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    printf("success 0\n");
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-
-    printf("success 1\n");
-
-    if (!window)
+    // Initialise GLFW
+    if( !glfwInit() )
     {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow( 1024, 768, "Tutorial 03 - Matrices", NULL, NULL);
+    if( window == NULL ){
+        fprintf( stderr, "Failed to open GLFW window.\n" );
         glfwTerminate();
         return -1;
     }
-
-    printf("success 2\n");
-
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    printf("success 3\n");
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-        /* Swap front and back buffers */
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = loadShaders(vertexShaderCode, fragmentShaderCode);
+
+    // Get a handle for our "MVP" uniform
+    GLint MatrixID = glGetUniformLocation(programID, "MVP");
+
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+    // Or, for an ortho camera :
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    glm::mat4 View       = glm::lookAt(
+            glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+            glm::vec3(0,0,0), // and looks at the origin
+            glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model      = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+    };
+    static const GLushort g_element_buffer_data[] = { 0, 1, 2 };
+
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    do{
+
+        // Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // Use our shader
+        glUseProgram(programID);
+
+        // Send our transformation to the currently bound shader,
+        // in the "MVP" uniform
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(
+                0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+                3,                  // size
+                GL_FLOAT,           // type
+                GL_FALSE,           // normalized?
+                0,                  // stride
+                (void*)0            // array buffer offset
+        );
+
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+        glDisableVertexAttribArray(0);
+
+        // Swap buffers
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
-    }
 
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0 );
+
+    // Cleanup VBO and shader
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteProgram(programID);
+    glDeleteVertexArrays(1, &VertexArrayID);
+
+    // Close OpenGL window and terminate GLFW
     glfwTerminate();
+
     return 0;
 }
+
